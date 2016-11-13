49730487fd21b7ea746b7887d12429e61448702e
Neil Schultz-Cox <novaparse@gmail.com>
2016-11-12 22:01:50 -0800

    Basic, initial week switching functionality implemented

---
+++ app/src/main/java/com/tinassist/neocbt/neocbt/Home.java
@@ -0,0 +1,31 @@
+package com.tinassist.neocbt.neocbt;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by novap on 11/12/2016.
+ */
+
+public class Home extends Fragment {
+
+    public Home() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.home, container, false);
+    }
+}
