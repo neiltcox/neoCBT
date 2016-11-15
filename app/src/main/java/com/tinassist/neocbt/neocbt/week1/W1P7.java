1f883c0cf056eb4d7c38edd3f0d5e2615ca36d7a
Cublai <jens.tuyls@icloud.com>
2016-11-14 22:33:16 -0800

    Added Java classes and corresponding xml files.

---
+++ app/src/main/java/com/tinassist/neocbt/neocbt/week1/W1P7.java
@@ -0,0 +1,32 @@
+package com.tinassist.neocbt.neocbt.week1;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.tinassist.neocbt.neocbt.R;
+
+/**
+ * Created by Jens on 11/13/16.
+ */
+
+public class W1P7 extends Fragment {
+    public W1P7() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.w1p7, container, false);
+    }
+}
