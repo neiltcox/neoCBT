73c5bf37051379cc717fa570c6e5b9a9fe9f6abf
kthirano <kthirano@uci.edu>
2016-11-16 13:30:46 -0800

    "" <- these are important

--- app/src/main/java/com/tinassist/neocbt/neocbt/surveys/PostSurvey1.java
+++ app/src/main/java/com/tinassist/neocbt/neocbt/surveys/PostSurvey1.java
@@ -24,31 +24,31 @@ public class PostSurvey1 extends Fragment {
     private int counter = 0;
     private int tempint = 0;
     private String[] anArray = new String[25];
-    anArray[0] = '1. Because of your tinnitus, is it difficult for you to concentrate?';
-    anArray[1] = '2. Does the loudness of your tinnitus make it difficult for you to hear people?';
-    anArray[2] = '3. Does your tinnitus make you angry?';
-    anArray[3] = '4. Does your tinnitus make you feel confused?';
-    anArray[4] = '5. Because of your tinnitus, do you feel desperate?';
-    anArray[5] = '6. Do you complain a great deal about your tinnitus?';
-    anArray[6] = '7. Because of your tinnitus, do you have trouble falling to sleep at night?';
-    anArray[7] = '8. Do you feel as though you cannot escape your tinnitus?';
-    anArray[8] = '9. Does your tinnitus interfere with your ability to enjoy your social activities\n(such as going out to dinner, to the movies)?';
-    anArray[9] = '10. Because of your tinnitus, do you feel frustrated?';
-    anArray[10] = '11. Because of your tinnitus, do you feel that you have a terrible disease?';
-    anArray[11] = '12. Does your tinnitus make it difficult for you to enjoy life?';
-    anArray[12] = '13. Does your tinnitus interfere with your job or household responsibilities?';
-    anArray[13] = '14. Because of your tinnitus, do you find that you are often irritable?';
-    anArray[14] = '15. Because of your tinnitus, is it difficult for you to read?';
-    anArray[15] = '16. Does your tinnitus make you upset?';
-    anArray[16] = '17. Do you feel that your tinnitus problem has placed stress on your relationships with members of your family and friends?';
-    anArray[17] = '18. Do you find it difficult to focus your attention away from your tinnitus and on other things?';
-    anArray[18] = '19. Do you feel that you have no control over your tinnitus?';
-    anArray[19] = '20. Because of your tinnitus, do you often feel tired?';
-    anArray[20] = '21. Because of your tinnitus, do you feel depressed?';
-    anArray[21] = '22. Does your tinnitus make you feel anxious?';
-    anArray[22] = '23. Do you feel that you can no longer cope with your tinnitus?';
-    anArray[23] = '24. Does your tinnitus get worse when you are under stress?';
-    anArray[24] = '25. Does your tinnitus make you feel insecure?';
+    anArray[0] = "1. Because of your tinnitus, is it difficult for you to concentrate?";
+    anArray[1] = "2. Does the loudness of your tinnitus make it difficult for you to hear people?";
+    anArray[2] = "3. Does your tinnitus make you angry?";
+    anArray[3] = "4. Does your tinnitus make you feel confused?";
+    anArray[4] = "5. Because of your tinnitus, do you feel desperate?";
+    anArray[5] = "6. Do you complain a great deal about your tinnitus?";
+    anArray[6] = "7. Because of your tinnitus, do you have trouble falling to sleep at night?";
+    anArray[7] = "8. Do you feel as though you cannot escape your tinnitus?";
+    anArray[8] = "9. Does your tinnitus interfere with your ability to enjoy your social activities (such as going out to dinner, to the movies)?";
+    anArray[9] = "10. Because of your tinnitus, do you feel frustrated?";
+    anArray[10] = "11. Because of your tinnitus, do you feel that you have a terrible disease?";
+    anArray[11] = "12. Does your tinnitus make it difficult for you to enjoy life?";
+    anArray[12] = "13. Does your tinnitus interfere with your job or household responsibilities?";
+    anArray[13] = "14. Because of your tinnitus, do you find that you are often irritable?";
+    anArray[14] = "15. Because of your tinnitus, is it difficult for you to read?";
+    anArray[15] = "16. Does your tinnitus make you upset?";
+    anArray[16] = "17. Do you feel that your tinnitus problem has placed stress on your relationships with members of your family and friends?";
+    anArray[17] = "18. Do you find it difficult to focus your attention away from your tinnitus and on other things"';
+    anArray[18] = "19. Do you feel that you have no control over your tinnitus?";
+    anArray[19] = "20. Because of your tinnitus, do you often feel tired?";
+    anArray[20] = "21. Because of your tinnitus, do you feel depressed?";
+    anArray[21] = "22. Does your tinnitus make you feel anxious?";
+    anArray[22] = "23. Do you feel that you can no longer cope with your tinnitus?";
+    anArray[23] = "24. Does your tinnitus get worse when you are under stress?";
+    anArray[24] = "25. Does your tinnitus make you feel insecure?";
     public PostSurvey1() {
         // Required empty public constructor
     }
@@ -85,7 +85,7 @@ public class PostSurvey1 extends Fragment {
         switch (view.getId()) {
             case R.id.radioButton:
                 if (checked) {
-                    tempint = 1;
+                    tempint = 4;
                 }
                 break;
             case R.id.radioButton2:
@@ -95,7 +95,7 @@ public class PostSurvey1 extends Fragment {
                 break;
             case R.id.radioButton3:
                 if (checked) {
-                    tempint = 3;
+                    tempint = 0;
                 }
                 break;
         }
