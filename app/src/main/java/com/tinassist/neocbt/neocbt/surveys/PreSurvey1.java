fa43603c9a23a3482398172224284fa1044fef68
kthirano <kthirano@uci.edu>
2016-11-20 18:47:04 -0800

    neil just link a new fragment and we can work from there LEL i cant print the results imma just comment out the place where we would switch frags pre survey 2 is done btw

--- app/src/main/java/com/tinassist/neocbt/neocbt/surveys/PreSurvey1.java
+++ app/src/main/java/com/tinassist/neocbt/neocbt/surveys/PreSurvey1.java
@@ -63,7 +63,7 @@ public class PreSurvey1 extends Fragment {
     }
 
 
-
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
