ff600abafec8a8ada9640d085fafce35dc67637f
Neil Schultz-Cox <novaparse@gmail.com>
2016-11-21 21:19:48 -0800

    Implemented all functional buttons but they still need formatting

--- app/src/main/java/com/tinassist/neocbt/neocbt/MainActivity.java
+++ app/src/main/java/com/tinassist/neocbt/neocbt/MainActivity.java
@@ -66,13 +66,6 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
@@ -112,7 +105,6 @@ public class MainActivity extends AppCompatActivity
             fragment = new W8P1();
         } else if (id == R.id.home) {
             fragment = new Home();
-            scheduleNotification(getNotification(), 60000)
         } else if (id == R.id.pre_survey1) {
             fragment = new PreSurvey1();
         } else if (id == R.id.pre_survey2) {
