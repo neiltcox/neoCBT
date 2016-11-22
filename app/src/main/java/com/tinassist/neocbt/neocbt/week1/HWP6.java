d4e4e7c5c4f8e1303f810f72e3fcb8f3384bd546
Kevin <gekevin1998@gmail.com>
2016-11-21 17:43:28 -0800

    Added Homework for Week 1 and Audio for Week 1

---
+++ app/src/main/java/com/tinassist/neocbt/neocbt/week1/HWP6.java
@@ -0,0 +1,34 @@
+package com.tinassist.neocbt.neocbt.week1;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.tinassist.neocbt.neocbt.R;
+
+/**
+ * Created by Kevin on 11/21/2016.
+ */
+
+
+public class HWP6 extends Fragment {
+
+    public HWP6() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.w1hwp6, container, false);
+    }
+}
---
+++ app/src/main/java/com/tinassist/neocbt/neocbt/week1/HWP7.java
@@ -0,0 +1,111 @@
+package com.tinassist.neocbt.neocbt.week1;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.SeekBar;
+
+import com.tinassist.neocbt.neocbt.R;
+
+/**
+ * Created by Kevin on 11/21/2016.
+ */
+
+
+public class HWP7 extends Fragment {
+
+
+    private MediaPlayer mySound;
+    private SeekBar mySeekbar;
+    Handler handler = new Handler();
+
+    public HWP7() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.w1hwp1);
+
+        mySound = MediaPlayer.create(this, R.raw.weekoneaudio);
+        mySeekbar = (SeekBar)findViewById(R.id.seekbar5);
+        mySeekbar.setOnSeekBarChangeListener(seekBarOnSeekChangeListener);
+        mySeekbar.setMax(mySound.getDuration());
+        update();
+
+    }
+
+    Runnable run = new Runnable() {
+        @Override
+        public void run() {
+            update();
+        }
+    };
+
+    public void update() {
+        mySeekbar.setProgress(mySound.getCurrentPosition());
+        handler.postDelayed(run, 1000);//Loops every second
+    }
+
+    SeekBar.OnSeekBarChangeListener seekBarOnSeekChangeListener = new SeekBar.OnSeekBarChangeListener() {
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            if (fromUser) {
+                mySound.seekTo(progress);
+                seekBar.setProgress(progress);
+            }
+        }
+    };
+
+    public void playpauseAudio(View view) {
+        if (mySound.isPlaying()) {
+            mySound.pause();
+        }
+        else {
+            mySound.start();
+        }
+    }
+
+    public void restartAudio(View view) {
+        if (mySound.isPlaying()) {
+            mySound.pause();
+            mySound.seekTo(0);
+            mySeekbar.setProgress(0);
+            mySound.start();
+        }
+    }
+
+    @Override
+    //Stops audio from playing and Runnable run from crashing when app is closed.
+    public void onPause() {
+        super.onPause();
+        handler.removeCallbacks(run);
+        mySound.release();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.w1hwp7, container, false);
+    }
+}
