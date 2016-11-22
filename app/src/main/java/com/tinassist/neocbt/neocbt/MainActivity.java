12f979ce7756d76bb31991ab0b3694748d696c69
Cublai <jens.tuyls@icloud.com>
2016-11-21 17:28:38 -0800

    Initial notifications for every 60 seconds, content through page 2 for every week, some styling

--- app/src/main/java/com/tinassist/neocbt/neocbt/MainActivity.java
+++ app/src/main/java/com/tinassist/neocbt/neocbt/MainActivity.java
@@ -1,6 +1,12 @@
 package com.tinassist.neocbt.neocbt;
 
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
@@ -22,6 +28,7 @@ import com.tinassist.neocbt.neocbt.week6.W6P1;
 import com.tinassist.neocbt.neocbt.week7.W7P1;
 import com.tinassist.neocbt.neocbt.week8.W8P1;
 
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -42,6 +49,7 @@ public class MainActivity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
 
         onNavigationItemSelected(navigationView.getMenu().getItem(0));
+
     }
 
     @Override
@@ -101,6 +109,7 @@ public class MainActivity extends AppCompatActivity
             fragment = new W8P1();
         } else if (id == R.id.home) {
             fragment = new Home();
+            scheduleNotification(getNotification(), 1000);
         } else if (id == R.id.pre_survey) {
             fragment = new PreSurvey1();
         }
@@ -121,4 +130,37 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    private void scheduleNotification(Notification notification, int delay) {
+        Intent notificationIntent = new Intent(this, NotificationPublisher.class);
+        notificationIntent.putExtra(NotificationPublisher.NOTIFICATION_ID, 1);
+        notificationIntent.putExtra(NotificationPublisher.NOTIFICATION, notification);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        long futureInMillis = SystemClock.elapsedRealtime() + delay;
+        AlarmManager alarmManager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, SystemClock.elapsedRealtime(),
+                60000, pendingIntent);
+        //alarmManager.set(AlarmManager.RTC_WAKEUP, futureInMillis, pendingIntent);
+    }
+
+    public Notification getNotification() {
+
+        Notification.Builder builder = new Notification.Builder(this);
+        builder.setContentTitle("Meditation Time!");
+        builder.setContentText("It's time for a new mindful breathing exercise!");
+        builder.setSmallIcon(R.drawable.ear1);
+        builder.setColor(0x4b966e);
+        return builder.build();
+    }
+
+    //public void original_notification () {
+      //  NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
+        //mBuilder.setSmallIcon(R.drawable.logo1);
+        //mBuilder.setContentTitle("Notification Alert, Click me!");
+        //mBuilder.setContentText("Hi, This is Android Notification Detail!");
+
+        //NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        //mNotificationManager.notify(1, mBuilder.build());
+    //}
 }
